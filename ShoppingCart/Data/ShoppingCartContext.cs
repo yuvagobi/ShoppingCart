d49c27b914f16b416d7bf8402bed29f9c72c226d
yuvagobi <yuvayuvasri@gmail.com>
2023-12-15 18:32:37 -0500

    pushing real time first time

---
+++ ShoppingCart/Data/ShoppingCartContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using ShoppingCart.Models;
+namespace ShoppingCart.Data
+
+{
+    public class ShoppingCartContext : DbContext
+    {
+        public ShoppingCartContext(DbContextOptions<ShoppingCartContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Category {get; set;} 
+    }
+}
